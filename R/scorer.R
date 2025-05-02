b68fa45d65194d8e03c4d044df9ef87efbf2eb7e
simonpcouch <simonpatrickcouch@gmail.com>
2025-05-02 12:52:51 -0500

    implement duration penalty with a 2-second "grace period"

--- R/scorer.R
+++ R/scorer.R
@@ -74,19 +74,27 @@ chores_scorer <- function(
   res <- full_res
 
   # tidy up + calculate numeric scores
-  res <- dplyr::mutate(res, across(everything(), ~ na_if(., "NA")))
+  res <- dplyr::mutate(res, across(everything(), ~ dplyr::na_if(., "NA")))
   grading <- res
 
+  # after a 2-second "grace period", subtract a point from the
+  # numerator per second
+  res$duration <- purrr::map_dbl(
+    samples$solver_chat,
+    duration_of_last_turn
+  )
   res <- dplyr::rowwise(res)
   res <- dplyr::mutate(
     res,
-    yes_count = sum(across(everything()) == "Yes", na.rm = TRUE),
-    # the sum would otherwise include `yes_count`
-    n = sum(!is.na(across(everything()))) - 1,
-    prop = case_when(
+    yes_count = sum(dplyr::across(dplyr::everything()) == "Yes", na.rm = TRUE),
+    # the sum would otherwise include `yes_count` and `duration`
+    n = sum(!is.na(dplyr::across(dplyr::everything()))) - 2,
+    duration_penalty = max(duration - 2, 0),
+    numerator = max(yes_count - duration_penalty, 0),
+    prop = dplyr::case_when(
       # all NAs, so result wasn't valid R code
       n == 0 ~ 0,
-      .default = yes_count / n
+      .default = numerator / n
     )
   )
 
@@ -135,4 +143,17 @@ is_valid_r_code <- function(x) {
     },
     error = function(e) FALSE
   )
+}
+
+duration_of_last_turn <- function(chat) {
+  turns <- chat$get_turns()
+
+  user_turn_completed <- turns[[length(turns) - 1]]@completed
+  assistant_turn_completed <- turns[[length(turns)]]@completed
+
+  as.numeric(difftime(
+    assistant_turn_completed,
+    user_turn_completed,
+    units = "secs"
+  ))
 }
